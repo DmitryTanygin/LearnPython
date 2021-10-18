665c5dcb8ec30d307e855a0f850cfbf71448aa00
DmitryTanygin <tanygin06@gmail.com>
2021-10-18 15:05:16 +0300

    Add task15_4

---
+++ task15/task15_4.py
@@ -0,0 +1,20 @@
+import re
+
+
+def get_ints_without_description(filename):
+    result = []
+    interface = ''
+    with open(filename) as f:
+        for line in f:
+            if line.startswith('interface'):
+                interface = re.search(r'interface (\S+)', line).group(1)
+            elif line.startswith(' description'):
+                interface = ''
+            elif line.startswith('!') and interface != '':
+                result.append(interface)
+                interface = ''
+        return result
+
+
+
+print(get_ints_without_description('config_r1.txt'))
