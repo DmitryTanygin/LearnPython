a5bd063d349886c751b0eccba29fb31b7103549f
DmitryTanygin <tanygin06@gmail.com>
2021-10-20 16:02:00 +0300

    Update task17_1

--- task17/task17_1.py
+++ task17/task17_1.py
@@ -2,21 +2,28 @@ import csv
 import re
 
 
-def write_dhcp_snooping_to_csv(filename, output):
-    switch = re.search(r'\S[^\_]+', filename).group()
-    with open(filename) as f, open(output, 'w') as g:
+def write_dhcp_snooping_to_csv(filenames, output):
+    result = []
+    headers = []
+    for filename in filenames:
+        switch = re.search(r'\S[^\_]+', filename).group()
+        with open(filename) as f:
+            regex = r'^\w+\:'
+            for line in f:
+                match = re.search(regex, line)
+                line = line.split()
+                if line[0] == 'MacAddress' and headers == []:
+                    line[0] = 'switch'
+                    headers = line
+                elif match:
+                    line[0] = switch
+                    result.append(line)
+    with open(output, 'w') as g:
         writer = csv.writer(g)
-        regex = r'^\w+\:'
-        for line in f:
-            match = re.search(regex, line)
-            line = line.split()
-            if line[0] == 'MacAddress':
-                line[0] = 'switch'
-                writer.writerow(line)
-            elif match:
-                line[0] = switch
-                writer.writerow(line)
+        writer.writerow(headers)
+        writer.writerows(result)
 
 
 if __name__ == '__main__':
-    write_dhcp_snooping_to_csv('sw1_dhcp_snooping.txt', 'output.csv')
+    write_dhcp_snooping_to_csv(['sw1_dhcp_snooping.txt', 'sw2_dhcp_snooping.txt', 'sw3_dhcp_snooping.txt'],
+                               'output.csv')
